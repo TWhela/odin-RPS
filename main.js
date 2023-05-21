d5eed348736794a2450870a150e0a5b5bb9ad416
TWhela <thomas.whelan@ntlworld.com>
2023-05-22 00:03:56 +0100

    carried out some styling and created js functions

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
+// Variables
 let playerChoice = "";
 let computerChoice = "";
 let playerScore = 0;
 let compScore = 0;
 let round = 0;
-let result = "";
+let finalResult = "";
 const options = ["rock", "paper", "scissors"];
+// Links
+const container = document.querySelector(".container");
+const rockButton = documenmt.getElementsByClassName("rock");
+const paperButton = documemt.getElementsByClassName("paper");
+const scissorsButtom = document.getElementsByClassName("scissors");
+const result = document.createElement('p');
+const computerScoreDisplay = document.getElementsByClassName("computer-score");
+const playerScoreDisplay = document.getElementsByClassName("player-score");
+// Functions
+function chooseRock() {
+    playerChoice = "rock";
+    return playerChoice;
+}
+function choosePaper() {
+    playerChoice = "paper";
+    return playerChoice
+}
+function chooseScissors() {
+    playerChoice = "scissors";
+    return playerChoice;
+}
+// Comparing Scores
